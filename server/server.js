a13b393b797bd29149b283b32dba8ac918ff0f6d
laurinknuenz <114426294+laurinknuenz@users.noreply.github.com>
2023-06-27 09:07:27 +0200

    Basic Login and Logout Works

--- server/server.js
+++ server/server.js
@@ -1,10 +1,71 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const spotify = require("../api/spotify-consumer");
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+var session = require("express-session");
 
 const app = express();
 
+const users = require("./users").users;
+
 app.use(bodyParser.json());
+
+passport.use(
+  new LocalStrategy(function verify(username, password, cb) {
+    let user = null;
+    users.forEach((loopUser) => {
+      if (loopUser.username === username) user = loopUser;
+    });
+
+    if (user === null || user.password !== password) {
+      console.log("Incorrect username or password.");
+      return cb(null, false, { message: "Incorrect username or password." });
+    }
+
+    console.log("Login succeeded!");
+    return cb(null, user);
+  })
+);
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+  return cb(
+    null,
+    users.find((user) => user.id === id)
+  );
+});
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "https://http.cat/401",
+  })
+);
+
+app.post("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 // Favorites list
 let favorites = [];
@@ -42,10 +103,28 @@ app.get("/favorites", (req, res) => {
 });
 
 // Root route
-app.get("/", function (req, res) {
+app.get("/", checkNotAuthenticated, function (req, res) {
   res.send('This is the Node.js backend server of the "Beat Buddy" App.');
 });
 
+app.get("/profile", checkAuthenticated, function (req, res) {
+  res.send("Hello and welcome to your profile, " + req.user.username + "!");
+});
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/profile");
+  }
+  next();
+}
+ 
 app.listen(3000, () => {
   console.log("The server is now listening on http://localhost:3000/");
 });
